6cced5e91463e94243e8dcac0b2b6893d725f506
xuan <1309484804@qq.com>
2021-04-20 15:30:13 +0800

    2-5 修改启动文案

--- src/main/java/com/zhuanye/wiki/WikiApplication.java
+++ src/main/java/com/zhuanye/wiki/WikiApplication.java
@@ -1,13 +1,29 @@
 package com.zhuanye.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+//启动日志
+//打印自定义配置信息，打印欢迎页，测试地址，或者提示性方案等等
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        //日志文件打印启动成功
+        LOG.info("启动成功！！");
+        //打印端口地址、端口号
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
