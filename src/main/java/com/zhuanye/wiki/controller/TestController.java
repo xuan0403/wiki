56c47bcb64516c214f36edb413c499732947fa30
xuan <1309484804@qq.com>
2021-04-20 19:57:46 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/zhuanye/wiki/controller/TestController.java
+++ src/main/java/com/zhuanye/wiki/controller/TestController.java
@@ -1,9 +1,7 @@
 package com.zhuanye.wiki.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller //用来返回页面
@@ -16,7 +14,7 @@ public class TestController {
     * /user？id=1
     * /user/1
     * @GetMapping 方法指接受get请求
-    * @PostMapping
+    * @PostMapping，浏览器不能访问post请求
     * @PutMapping
     * @DeleteMapping
     * @RequestMapping(value="/user/1",method = RequestMethod.GET)  //注解，表示这个接口只支持GET请求方式
@@ -24,7 +22,15 @@ public class TestController {
     //@RequestMapping("/hello")  //注解，表示这个接口支持所有的请求方式
     @GetMapping("/hello")
     public String hello(){
+
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+   /* public String helloPost(Map<String,String> map){*/  //参数接收不到
+    public String helloPost(String name){
+
+        return "Hello World!Post" +name;
     }
 
 }
