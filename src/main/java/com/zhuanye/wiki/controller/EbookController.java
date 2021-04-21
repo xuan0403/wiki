ec346c327e505b1614ff1c9d3c14699173646c2c
xuan <1309484804@qq.com>
2021-04-21 23:59:14 +0800

    3-6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/zhuanye/wiki/controller/EbookController.java
+++ src/main/java/com/zhuanye/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.zhuanye.wiki.controller;
 
 import com.zhuanye.wiki.domain.Ebook;
+import com.zhuanye.wiki.resp.CommonResp;
 import com.zhuanye.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,17 @@ public class EbookController {
     private EbookService ebookService;
 
 
-    @GetMapping("/list")
+    /*@GetMapping("/list")
     public List<Ebook> list(){
         return ebookService.list();
+    }*/
+
+    @GetMapping("/list")
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();//这里的凡星是实际返回业务数据的类型，及content的类型
+        List<Ebook> list= ebookService.list();
+        resp.setContent(list);            //在CommonResp类里success默认的是true
+        return resp;
     }
 
 }
---
+++ src/main/java/com/zhuanye/wiki/resp/CommonResp.java
@@ -0,0 +1,54 @@
+package com.zhuanye.wiki.resp;
+
+//通用返回类
+public class CommonResp<T> {
+
+    /**
+     * 业务上的成功或失败
+     */
+    private boolean success = true;
+
+    /**
+     * 返回信息
+     */
+    private String message;   //错误弹出信息message
+
+    /**
+     * 返回泛型数据，自定义类型
+     */
+    private T content;       //登录成功后的用户信息
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ResponseDto{");
+        sb.append("success=").append(success);
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", content=").append(content);
+        sb.append('}');
+        return sb.toString();
+    }
+}
