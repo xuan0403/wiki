f37ad08f1aaa48b1a6cf46045e2205e3ca848f8a
xuan <1309484804@qq.com>
2021-04-25 14:16:28 +0800

    5-4 集成图标组件 npm install @ant-design/icons-vue@5.1.8 --save

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,8 +4,16 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
 /*通过main.ts将App.vue和index.html关联起来
 * vue cli初始执行main.ts，将页面App.vue内容渲染到index.html，完成页面显示
 * */
+
+// 全局使用图标
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
